3a4552192c946056acad4ed7503b660ec9ddfd12
Davi Batista <dssb.app@gmail.com>
2022-02-15 14:48:50 -0300

    atualizando readme

---
+++ Bootcamp HTML Web Developer/Códigos/aula-javascript/js/main.js
@@ -0,0 +1,13 @@
+var nome = "Davi Sousa Sena Batista";
+var n1 = 32;
+var n2 = 10;
+var frase = "Baixa Grande-BA";
+var lista = ["uva","laranja","banana"];
+lista.push("melão");
+lista.pop()
+
+//alert("Bem vindo, " + nome)
+alert(n1*n2)
+
+console.log(lista[1]);
+console.log(lista.l
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dio.me
 Repositório do aprendizado na plataforma da Digital Innovation One
+
+### Links Úteis
+## Aprender CSS3 Jogando
+http://www.flexboxdefense.com/
